a3cb45a22910e679c3f9361529c110bec9308145
RadifTajwar <radiftajwarmahi420@gmail.com>
2025-08-02 19:12:54 +0600

    Initial Code Setup With Next,typescript and shadCN component

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import {TestComponent} from "@/features/test";
+export default function Home() {
+  return (
+    <div className="abc">
+      <TestComponent >
+        
+      </TestComponent>
+    </div>
+  );
+}
